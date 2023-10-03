239f8c38262bc65c1f79d2831e117496cfd8b029
amulet1989 <muletdelosreyes89@gmail.com>
2023-10-03 09:43:00 -0300

    modificated for fine tune

--- src/config.py
+++ src/config.py
@@ -5,14 +5,14 @@ HOME = str(pathlib.Path(__file__).parent.parent)
 
 DATASET_DIR_PATH = os.path.join(HOME, "dataset")
 
-DATA_MERGED_DIR_PATH = os.path.join(DATASET_DIR_PATH, "Merged_Dataset")
+DATA_MERGED_DIR_PATH = os.path.join(DATASET_DIR_PATH, "Merged_Dataset")  # No
 
-DATA_AUMENTED_DIR_PATH = os.path.join(DATASET_DIR_PATH, "Augmented_Dataset")
+DATA_AUMENTED_DIR_PATH = os.path.join(DATASET_DIR_PATH, "Merged_Dataset")
 
-DATA_YAML_PATH = os.path.join(DATASET_DIR_PATH, "Augmented_Dataset", "data.yaml")
+DATA_YAML_PATH = os.path.join(DATASET_DIR_PATH, "Merged_Dataset", "data.yaml")
 
 DATA_ZIP_PATH = os.path.join(DATASET_DIR_PATH, "data.zip")
 
 AWS_BUCKET = "probando-entrenar-una-red-en-aws"
 
-AWS_FILE_PATH = "Train_Datasets_MiniGO_products/Augmented_Dataset.zip"
+AWS_FILE_PATH = "Train_Datasets_MiniGO_products/Merged_Dataset.zip"
